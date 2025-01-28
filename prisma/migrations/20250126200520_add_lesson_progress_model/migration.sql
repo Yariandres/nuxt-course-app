868f57428a337e5878897dcd9b5830606bfe77dc
Yari Herrera <yari.andres@gmail.com>
2025-01-28 18:05:42 +0100

    updates prisma

---
+++ prisma/migrations/20250126200520_add_lesson_progress_model/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - The primary key for the `Lesson` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Lesson` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "Lesson" DROP CONSTRAINT "Lesson_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "Lesson_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "LessonProgress" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "lessonId" INTEGER NOT NULL,
+    "userEmail" TEXT NOT NULL,
+
+    CONSTRAINT "LessonProgress_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LessonProgress_lessonId_userEmail_key" ON "LessonProgress"("lessonId", "userEmail");
+
+-- AddForeignKey
+ALTER TABLE "LessonProgress" ADD CONSTRAINT "LessonProgress_lessonId_fkey" FOREIGN KEY ("lessonId") REFERENCES "Lesson"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
